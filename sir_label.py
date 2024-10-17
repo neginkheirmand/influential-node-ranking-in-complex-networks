caa77c2418b3f1698fe743c5ac8cfd0bda9a77c6
neginkheirmand <neginkheirmand@gmail.com>
2024-10-17 07:52:01 +0330

    just adding checkpoint in case i want turn back later

--- sir_label.py
+++ sir_label.py
@@ -4,6 +4,7 @@ import ndlib.models.epidemics as ep
 import numpy as np
 import os
 import csv
+from pathlib import Path
 
 # import matplotlib.pyplot as plt
 # from ndlib.viz.mpl.DiffusionTrend import DiffusionTrend
@@ -97,11 +98,14 @@ def get_sir_dict(sir_of_graph, affected_scales, node):
         sir_of_graph[b].append((node, affected_scales[b]))
     return sir_of_graph
 
-def Sir_of_graph(path):
-    G = nx.read_edgelist(path, comments="%", nodetype=int)
+
+
+# path should be something 
+def Sir_of_graph(graph_path, result_path = './datasets/SIR_Results/'):
+    G = nx.read_edgelist(graph_path, comments="%", nodetype=int)
     B_values =get_B_Value(G)
     sir_of_graph = {b: [] for b in B_values}   # a dict containing list of tuples 
-    i = 0
+    # paths
     for node in sorted(G.nodes()):
         # process node
         infected = {node: 1}
@@ -109,11 +113,8 @@ def Sir_of_graph(path):
         sir_of_graph = get_sir_dict(sir_of_graph, affected_scales, node)
         print(sir_of_graph)
 
-        i+=1
-        if i ==2:
-            break
 
-save_tuple((1, 1.64384), 'a.csv')
+# save_tuple((1, 1.64384), 'a.csv')
 
 # Sir_of_graph('./datasets/BA_EXP/ba_edgelist_exp3_4000_10.edges')
 
@@ -123,24 +124,54 @@ save_tuple((1, 1.64384), 'a.csv')
 # affected_scales = SIR(G, {1: 1}, B_values)
 # print(affected_scales)
 
-# directory = "./datasets/"
-# graph_list = []
-# for dirpath, _, files in os.walk(directory):
-#     for filename in files:
-#         try:
-#             if filename.endswith(".edges"):
-#                 file_path = os.path.join(dirpath, filename) 
-#                 graph_list.append(file_path)
-#                 graph = nx.read_edgelist(file_path, comments="%", nodetype=int)
-#                 b_list = sir_model(graph, 0.1,5,1000)
-#                 b_dict = get_Bdict_from_Blist(b_list)
-#                 i = 0
-#                 for b in b_dict.keys():
-#                     b_dict[b]=sorted(b_dict[b], key=lambda x: x[0])
-#                     x_  = [t[0] for t in b_dict[b]]
-#                     y_  = [t[1] for t in b_dict[b]]
-#                     Sir = pd.DataFrame({'Node':x_,'SIR':y_})
-#                     Sir.to_csv(f'{filename}.csv',index=False)
-#                     i+=1
-#         except Exception as e: 
-#             print(e, f'{filename}')
+
+
+
+def create_SIR_dir(graph_name, result_path = './datasets/SIR_Results/'):
+    sir_dir =os.path.join(result_path, graph_name)
+    if not folder_exists(sir_dir):
+        print("creating: ", sir_dir)
+        sir_dir = Path(sir_dir)
+        sir_dir.mkdir(parents=True)
+
+
+def get_graph_paths(dataset_dir= "./datasets/"):
+    graph_list = []
+    for dirpath, _, files in os.walk(dataset_dir):
+        for filename in files:
+            try:
+                if filename.endswith(".edges"):
+                    file_path = os.path.join(dirpath, filename) 
+                    graph_list.append((file_path, os.path.splitext(filename)[0]))
+                    # print(file_path)
+                    # print(filename)
+                    # print("hhhh")
+                    # graph = nx.read_edgelist(file_path, comments="%", nodetype=int)
+                    # b_list = sir_model(graph, 0.1,5,1000)
+                    # b_dict = get_Bdict_from_Blist(b_list)
+                    # i = 0
+                    # for b in b_dict.keys():
+                    #     b_dict[b]=sorted(b_dict[b], key=lambda x: x[0])
+                    #     x_  = [t[0] for t in b_dict[b]]
+                    #     y_  = [t[1] for t in b_dict[b]]
+                    #     Sir = pd.DataFrame({'Node':x_,'SIR':y_})
+                    #     Sir.to_csv(f'{filename}.csv',index=False)
+                    #     i+=1
+            except Exception as e: 
+                print(e, f'{filename}')
+    return graph_list
+
+def main():
+    graph_list = get_graph_paths()
+    result_path = './datasets/SIR_Results/'
+    for (g_path, g_name) in graph_list:
+        create_SIR_dir(g_name, result_path)
+
+    for (g_path, g_name) in graph_list:
+        
+    
+    
+
+    
+
+main()
