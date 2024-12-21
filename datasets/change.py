541a3221a26dc65f98460b306b525c4fc037904b
neginkheirmand <neginkheirmand@gmail.com>
2024-12-21 21:12:22 +0330

    add function

--- datasets/change.py
+++ datasets/change.py
@@ -3,16 +3,20 @@ import os
 input_directory = "./"
 output_directory = "./"
 
-for dirpath, _, files in os.walk(input_directory):
-    try:
-        # with open('./arenas-pgp.edge', 'r') as f:
-        # with open('./politician_edges.csv', 'r') as f:
-        with open('./tvshow_edges.edge', 'r') as f:
-            lines = f.readlines()
-            corrected_lines = [line.replace(',', ' ').strip() for line in lines]
+def edge2edges_file(edge_file, edges_file):
+    for dirpath, _, files in os.walk(input_directory):
+        try:
+            with open(edge_file, 'r') as f:
+                lines = f.readlines()
+                corrected_lines = [line.replace(',', ' ').strip() for line in lines]
 
-        with open('./tvshow_edges.edges', 'w') as f:
-            f.write('\n'.join(corrected_lines) + '\n')  
+            with open(edges_file, 'w') as f:
+                f.write('\n'.join(corrected_lines) + '\n')  
 
-    except Exception as e:
-        print(f"Error processing ./tvshow_edges.csv: {e}")
+        except Exception as e:
+            print(f"Error processing ./tvshow_edges.csv: {e}")
+
+
+edges_file = './tvshow_edges.edges'
+edge_file = './tvshow_edges.edge'
+edge2edges_file(edge_file, edges_file)
